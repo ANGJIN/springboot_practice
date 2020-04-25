250d8f31b4380833207f12c0853ac863200a6930
김영진 <zmfjrxmfjr@naver.com>
2020-04-25 15:46:44 +0900

    dto with lombok

--- src/main/java/com/example/org/springboot_practice/web/dto/HelloResponseDto.java
+++ src/main/java/com/example/org/springboot_practice/web/dto/HelloResponseDto.java
@@ -1,4 +1,11 @@
 package com.example.org.springboot_practice.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
