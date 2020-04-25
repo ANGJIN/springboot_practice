5f00261595c7069ecdc7bc70f5633af5169e20bc
김영진 <zmfjrxmfjr@naver.com>
2020-04-25 21:22:31 +0900

    Auditing Feature

--- src/main/java/com/example/org/springboot_practice/domain/posts/Posts.java
+++ src/main/java/com/example/org/springboot_practice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.org.springboot_practice.domain.posts;
 
+import com.example.org.springboot_practice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,5 +29,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
