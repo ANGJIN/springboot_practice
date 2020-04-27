23960f197de7c8e5fe81bb8460ba080a957f237e
김영진 <zmfjrxmfjr@naver.com>
2020-04-27 22:06:04 +0900

    google OAuth2.0

--- src/main/java/com/example/org/springboot_practice/web/IndexController.java
+++ src/main/java/com/example/org/springboot_practice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.org.springboot_practice.web;
 
+import com.example.org.springboot_practice.config.auth.LoginUser;
+import com.example.org.springboot_practice.config.auth.dto.SessionUser;
 import com.example.org.springboot_practice.service.posts.PostsService;
 import com.example.org.springboot_practice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
